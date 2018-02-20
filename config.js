0ab877e73c674a4e45732c73e8e90a5476143fc8
mattNat <mattsanpedro76@gmail.com>
2018-02-19 16:25:05 -0800

    add authorization to credentials

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/local-auth-demo';
+exports.DATABASE_URL = process.env.DATABASE_URL 
+  || 'mongodb://admin:admin123@localhost/local-auth-demo?authSource=admin'
+  || 'mongodb://localhost/local-auth-demo';
 exports.PORT = process.env.PORT || 8080;
